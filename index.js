12ac826ba660ce549d85d60a3738aa14ea9249f2
BST53 <burhan.s.tinwala@gmail.com>
2023-08-23 12:52:47 +0530

    "Added Responsiveness for Mobiles"

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3000;
 
 //Routes
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/homepage"));
 app.use("/India", require("./routes/state"));
 app.listen(port);
